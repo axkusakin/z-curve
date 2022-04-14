4f4fcd4b9bf4255157c6b6efe38a81ac482cc703
axkusakin <axkusakin@gmail.com>
2022-04-15 01:01:06 +0300

    feat: update z-curve script

--- z_curve.py
+++ z_curve.py
@@ -1,12 +1,12 @@
 """This module calculates and plots z-curve (https://doi.org/10.1006/jtbi.1997.0401)"""
 
+import argparse
 import pandas as pd
 import plotly.express as px
 
 
 def delete_description(file):
     """Delete description from FASTA file.
-    
     Doesn't work on multifasta.
     """
     total_sequence = []
@@ -22,7 +22,6 @@ def delete_description(file):
 
 def genome_coordinates(genome):
     """Counts coordinates.
-    
     x = R-Y (purine - pyrimidine),
     y = M-K (amino - keto),
     z = W-S (strongH bond - weak-H bond).
@@ -65,10 +64,9 @@ def plot_maker(genome):
 
 # Futher starts code for genome divided into sequences.
 
-def split_genome(genome, sequence_length):
+def split_genome(genome, sequence_length=10):
     """Divides the genome into sequences of a given length (sequence_length)
     and averages the parameter (x, y, and z) for each.
-
     Needed for large genomes, to plot faster.
     ERROR: If genome length is not divisible by sequence_length, the last nucleotides are dropped.
     """
@@ -81,7 +79,6 @@ def split_genome(genome, sequence_length):
 
 def splitted_genome_coordinates(splitted_genome):
     """Coordinates for the genome divided into sequences.
-    
     The function is needed in order to reduce the number of coordinates to construct the z-curve.
     """
     count_x, count_y, count_z = 0, 0, 0
@@ -108,3 +105,33 @@ def splitted_genome_coordinates(splitted_genome):
         coordinates['y'].append(count_y * 2)
         coordinates['z'].append(count_z * 2)
     return coordinates
+
+def main(settings):
+    """Analyse input data."""
+    input_file = settings["input_files"]
+    output_file = settings["output_files"]
+    algorithm = settings["algorithm"]
+
+    z_curve_plot = plot_maker(genome_coordinates(delete_description(input_file)))
+    # output_z_curve(output_file)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Plots z-curve.")
+    parser.add_argument('-i', "--input", help="Genome file in fasta format.", required=True)
+    parser.add_argument('-o', "--output", help="File name for the graph")
+    parser.add_argument('-f', "--fast", help="Apply a fast algorithm to calculate coordinates", 
+                                                                    required=False, default=False)
+    args = parser.parse_args()
+
+    input_file = args["input"]
+    output_file = args["output"]
+    algorithm = args["fast"]
+
+    settings = {
+        "input_files": input_file,
+        "output_file": output_file,
+        "algorithm": algorithm
+    }
+
+    main(settings)
